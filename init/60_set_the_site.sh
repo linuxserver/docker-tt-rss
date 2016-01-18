b2e6745b4724eb3eae367d517fad1200a880c3e0
sparklyballs <sparklyballs@gmail.com>
2016-01-18 10:02:31 +0000

    add opt out for autoupdates to git update init

--- init/60_set_the_site.sh
+++ init/60_set_the_site.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 [[ ! -d /config/www/tt-rss/.git ]] && (echo "fetching tt-rss files, this may take a little while" && \
-git clone https://tt-rss.org/git/tt-rss.git /config/www/tt-rss)
+git clone https://tt-rss.org/git/tt-rss.git /config/www/tt-rss && chown -R abc:abc /config/www)
+
+# opt out for autoupdates
+[ "$ADVANCED_DISABLEUPDATES" ] && exit 0
 
 cd /config/www/tt-rss
 git pull origin master
-
 chown -R abc:abc /config/www
 
